ff4522c9fc4007eb5e789f68efcd31d2e1237e01
Leonard Axelsson <leo@xlson.com>
2012-03-02 15:48:23 +0100

    Slides from graphite presentation at Crash & Burn 2012.

---
+++ presentations/2012/03/02/graphite-crash-and-burn-2012/file/custom_script.js
@@ -0,0 +1,23 @@
+DEFAULT_BACKGROUND = "image/background.png"
+
+function set_background(src) {
+    var old_bg = $('body').css('background-image');
+    $('body').css('background-image', old_bg).removeClass('fullScreen');
+    $('body')
+        .css('background-image', 'url(' + src + ')')
+        .addClass('fullScreen');
+}
+
+$(function() {
+  $('div.content').live('showoff:show', function(evt) {
+    var slide_bg_img = $('img[alt=background]', evt.target);
+    if (slide_bg_img.size() > 0) {
+      var src = slide_bg_img.attr('src');
+      slide_bg_img.hide();
+      set_background(src);
+    } else {
+      set_background(DEFAULT_BACKGROUND);
+    }
+  });
+});
+
